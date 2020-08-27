ebef248731e7c6fa20781291c3e850f80bbd5c01
Philipp Ossler <philipp.ossler@gmail.com>
2020-08-27 11:49:04 +0200

    test(tck): more specs

--- src/test/kotlin/io/zeebe/bpmn/tck/BpmnTest.kt
+++ src/test/kotlin/io/zeebe/bpmn/tck/BpmnTest.kt
@@ -8,13 +8,13 @@ import io.zeebe.bpmnspec.runner.zeebe.ZeebeTestRunner
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.params.ParameterizedTest
 
-@BpmnSpecRunner
+@BpmnSpecRunner(resourceDirectory = "workflows")
 class BpmnTest(factory: SpecRunnerFactory) {
 
     private val specRunner = factory.create(testRunner = ZeebeTestRunner())
 
     @ParameterizedTest
-    @BpmnSpecSource(specResource = "exclusive-gateway-spec.yaml")
+    @BpmnSpecSource(specResources = ["exclusive-gateway-spec.yaml"])
     fun `exclusive gateway`(spec: BpmnSpecTestCase) {
 
         val testResult = specRunner.runSingleTestCase(resources = spec.resources, testcase = spec.testCase)
@@ -25,7 +25,18 @@ class BpmnTest(factory: SpecRunnerFactory) {
     }
 
     @ParameterizedTest
-    @BpmnSpecSource(specResource = "boundary-event-spec.yaml")
+    @BpmnSpecSource(specResources = ["parallel-gateway-spec.yaml"])
+    fun `parallel gateway`(spec: BpmnSpecTestCase) {
+
+        val testResult = specRunner.runSingleTestCase(resources = spec.resources, testcase = spec.testCase)
+
+        Assertions.assertThat(testResult.success)
+                .describedAs(testResult.message)
+                .isTrue()
+    }
+
+    @ParameterizedTest
+    @BpmnSpecSource(specResources = ["boundary-event-spec.yaml"])
     fun `boundary event`(spec: BpmnSpecTestCase) {
 
         val testResult = specRunner.runSingleTestCase(resources = spec.resources, testcase = spec.testCase)
